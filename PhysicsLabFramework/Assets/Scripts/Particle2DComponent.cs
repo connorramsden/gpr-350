620579e328f9e82cde8167398b2a5e5e71e47584
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2019-09-08 13:04:47 -0400

    ECS Architecture mostly achieved?

---
+++ PhysicsLabFramework/Assets/Scripts/Particle2DComponent.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Particle2DComponent : MonoBehaviour
+{
+    // Position Components
+    public Vector2 position, velocity, acceleration;
+    // Rotation Components
+    public float rotation, angularVelocity, angularAccel;
+
+    [Header("Move || Rotate")]
+    public bool shouldMove;
+    public bool shouldRotate;
+
+    // Bonus Bell's and Whistles
+    public enum IntegrationType
+    {
+        EULER,
+        KINEMATIC
+    }
+
+    [Header("Integration Type")]
+    public IntegrationType currentIntegration;
+
+    // Updates a particle's position based on current i+ntegration type
+    public void UpdatePosition(float dt)
+    {
+        // Utilize Euler Explicit Integration formula
+        // x(t+dt) = x(t) + v(t)dt
+        if (currentIntegration.Equals(IntegrationType.EULER))
+        {
+            position += velocity * dt;
+        }
+        // Use the Kinematic formula for movement integration
+        // x(t+dt) = x(t) + v(t)dt + 1/2(a(t)dt^2)
+        else if (currentIntegration.Equals(IntegrationType.KINEMATIC))
+        {
+            position += (velocity * dt) + (0.5f * acceleration * (dt * dt));
+        }
+
+        velocity += acceleration * dt;
+
+        // step4
+        // test by faking motion along a curve
+        acceleration.x = -Mathf.Sin(Time.time);
+
+        transform.position = new Vector3(position.x, transform.position.y);
+    }
+
+    public void UpdateRotation(float dt)
+    {
+        // Utilize Euler Explicit Integration formula for rotation
+        if (currentIntegration.Equals(IntegrationType.EULER))
+        {
+            rotation += angularVelocity * dt;
+        }
+        // Utilize Kinematic formula for rotation integration
+        else if (currentIntegration.Equals(IntegrationType.KINEMATIC))
+        {
+            rotation += (angularVelocity * dt) + (0.5f * angularAccel * (dt * dt));
+        }
+
+        angularVelocity += angularAccel * dt;
+
+        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, rotation));
+    }
+
+    private void Awake()
+    {
+        // Upon Awake(), set the object's tag to Particle
+        // if it is not already set
+        if (!gameObject.CompareTag("Particle"))
+        {
+            gameObject.tag = "Particle";
+        }
+    }
+}
---
+++ PhysicsLabFramework/Assets/Scripts/Particle2DSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2DSystem : MonoBehaviour
+{
+    List<GameObject> particleList;
+
+    // Trying to mimic ECS-style updating all entities
+    private void UpdateAllParticles(float dt)
+    {
+        foreach(GameObject particle in particleList)
+        {
+            Particle2DComponent p2d = particle.GetComponent<Particle2DComponent>();
+
+            if(p2d.shouldMove)
+                p2d.UpdatePosition(dt);
+            if(p2d.shouldRotate)
+                p2d.UpdateRotation(dt);           
+        }
+    }
+
+    private void Awake()
+    {
+        // Initialize particleList
+        particleList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        // Snag all GO's with tag Particle for particleList
+        particleList.AddRange(GameObject.FindGameObjectsWithTag("Particle"));
+    }
+
+    private void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+        
+        UpdateAllParticles(dt);
+    }
+}
