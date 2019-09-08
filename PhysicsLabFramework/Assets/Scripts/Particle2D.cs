5034f9748a7fc1b7d0bf8e4ea82c1e39673153dc
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2019-09-08 10:49:41 -0400

    Starting to update towards ECS-style architecture

--- PhysicsLabFramework/Assets/Scripts/Particle2D.cs
+++ PhysicsLabFramework/Assets/Scripts/Particle2D.cs
@@ -11,18 +11,18 @@ public class Particle2D : MonoBehaviour
         KINEMATIC
     }
 
+    struct Particle
+    {
+        public Vector2 position, velocity, acceleration;
+        public float rotation;
+        public float angularVelocity;
+        public float angularAccel;
+    }
+
+    Particle integratedParticle;
+
     [Header("Integration Type")]
     public IntegrationType currentIntegration;
-
-    // Step 1
-    [Header("Position Properties")]
-    public Vector2 position;
-    public Vector2 velocity;
-    public Vector2 acceleration;
-    [Header("Rotation Properties")]
-    public float rotation;
-    public float angularVelocity;
-    public float angularAccel;
 
     // step 2
     void UpdatePositionExplicitEuler(float dt)
@@ -32,37 +32,39 @@ public class Particle2D : MonoBehaviour
         // Euler: For the current value, add the derivative
         // F(t+dt) = F(t) + f(t)dt
         //                + (dF/dt) dt
-        position += velocity * dt;
+        integratedParticle.position += integratedParticle.velocity * dt;
 
         // v(t+dt) = v(t) + a(t)dt
-        velocity += acceleration * dt;
+        integratedParticle.velocity += integratedParticle.acceleration * dt;
     }
 
     void UpdatePositionKinematic(float dt)
     {
         // x(t+dt) = x(t) + v(t)dt + 0.5(a(t)dt^2)
-        position += (velocity * dt) + (0.5f * acceleration * (dt * dt));
+        integratedParticle.position += (integratedParticle.velocity * dt) + (0.5f * integratedParticle.acceleration * (dt * dt));
 
-        velocity += acceleration * dt;
+        integratedParticle.velocity += integratedParticle.acceleration * dt;
     }
 
     void UpdateRotationEulerExplicit(float dt)
     {
-        rotation += angularVelocity * dt;
+        integratedParticle.rotation += integratedParticle.angularVelocity * dt;
 
-        angularVelocity += angularAccel * dt;
+        integratedParticle.angularVelocity += integratedParticle.angularAccel * dt;
     }
 
     void UpdateRotationKinematic(float dt)
     {
-        rotation += (angularVelocity * dt) + (0.5f * angularAccel * (dt * dt));
+        integratedParticle.rotation += (integratedParticle.angularVelocity * dt) + (0.5f * integratedParticle.angularAccel * (dt * dt));
 
-        angularVelocity += angularAccel * dt;
+        integratedParticle.angularVelocity += integratedParticle.angularAccel * dt;
     }
 
     private void FixedUpdate()
     {
         float dt = Time.fixedDeltaTime;
+
+        Debug.Log($"{gameObject.name}'s integration type is {currentIntegration}");
 
         // step 3
         // choose integrator
@@ -78,13 +80,13 @@ public class Particle2D : MonoBehaviour
         }
 
         // update transform
-        transform.position = new Vector3(position.x, transform.position.y);
+        transform.position = new Vector3(integratedParticle.position.x, transform.position.y);
         // Debug.Log($"{gameObject.name}'s position is {gameObject.transform.position.x}");
         // update rotation
-        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, rotation));
+        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, integratedParticle.rotation));
 
         // step4
         // test by faking motion along a curve
-        acceleration.x = -Mathf.Sin(Time.time);
+        integratedParticle.acceleration.x = -Mathf.Sin(Time.time);
     }
 }
