a73dd4e512ba9f20d0f6892bf333172742419bd8
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2019-11-25 18:11:06 -0500

    uhhh

--- UnityLib/src/lib.rs
+++ UnityLib/src/lib.rs
@@ -4,6 +4,7 @@ extern crate prost;
 extern crate prost_derive;
 
 use ffi_support::ExternError;
+use std::os::raw::c_int;
 
 pub mod phys {
     include!(concat!(env!("OUT_DIR"), "/phys.rs"));
@@ -13,18 +14,29 @@ pub mod phys {
 /// This is dealing with pointers, and its unsafe!
 /// Don't expect usual Rust safety coming into this function
 #[no_mangle] // Ensures that we can call this from C#
-pub unsafe extern "C" fn ulib_check_collisions(data: *const u8, len: i32, error: &mut ExternError) {
-    ffi_support::call_with_result(error, || {
-        assert!(len >= 0, "Bad buffer len: {}", len);
-        let bytes = if len == 0 {
-            &[]
-        } else {
-            assert!(!data.is_null(), "Unexpected null data pointer");
-            std::slice::from_raw_parts(data, len as usize)
-        };
+pub unsafe extern "C" fn ulib_check_collisions(data: *const u8, len: i32, error: &mut ExternError){
 
-        let my_thing: phys::Ch3d = prost::Message::decode(bytes)?;
+    assert!(len >= 0, "Bad buffer len: {}", len);
+    let bytes = if len == 0 {
+        &[]
+    } else {
+        assert!(!data.is_null(), "Unexpected null data pointer");
+        std::slice::from_raw_parts(data, len as usize);
+    };
 
-        Ok(())
-    })
+    let my_thing: phys::Ch3d = prost::Message::decode(bytes)?;
+
+//    ffi_support::call_with_result(error, || {
+//        assert!(len >= 0, "Bad buffer len: {}", len);
+//        let bytes = if len == 0 {
+//            &[]
+//        } else {
+//            assert!(!data.is_null(), "Unexpected null data pointer");
+//            std::slice::from_raw_parts(data, len as usize)
+//        };
+//
+//        let my_thing: phys::Ch3d = prost::Message::decode(bytes)?;
+//
+//        Ok(())
+//    })
 }
