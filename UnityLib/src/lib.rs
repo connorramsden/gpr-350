42450be9b9ca1d40448f5fba619f379b62ea5ba6
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2019-11-24 19:35:59 -0500

    im so tired

--- UnityLib/src/lib.rs
+++ UnityLib/src/lib.rs
@@ -1,17 +1,27 @@
 extern crate bytes;
+extern crate ffi_support;
 extern crate prost;
 extern crate prost_derive;
-extern crate ffi_support;
 
-use std::os::raw::c_int;
-use ffi_support::{FfiStr};
+use ffi_support::ExternError;
 
 pub mod phys {
-    include!(concat!(env!("OUT_DIR"), "/phys.rs"));
+	include!(concat!(env!("OUT_DIR"), "/phys.rs"));
 }
 
+/// # Safety
+/// This is dealing with pointers, and its unsafe!
+/// Don't expect usual Rust safety coming into this function
 #[no_mangle] // Ensures that we can call this from C#
-pub extern "C" fn check_collisions(foreign_hull: FfiStr) -> c_int {
+pub unsafe extern "C" fn ulib_check_collisions(data: *const u8, len: i32, error: &mut ExternError) {
+	ffi_support::call_with_result(error, || {
+		let bytes = if len == 0 {
+			&[]
+		} else {
+			assert!(!data.is_null(), "Unexpected null data pointer");
+			std::slice::from_raw_parts(data, len as usize)
+		};
 
-    0
+		Ok(())
+	})
 }
